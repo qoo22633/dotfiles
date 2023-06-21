fa00af0fa5504d7d5d19e806bdbc06b639b70b36
yudai.yamashita <yama226.33@gmail.com>
2023-06-21 22:53:35 +0900

    Update: init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,57 @@
-print('hello, init.lua')
+-- encoding
+vim.o.encofing = 'utf-8'
+vim.scriptencoding = 'utf-8'
 
+-- visual
+vim.o.ambiwidth = 'double'
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+
+vim.o.visualbell = true
+vim.o.number = true
+vim.o.showmatch = true
+vim.o.matchtime = 1
+
+-- search
+vim.o.incsearch = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.hlsearch = true
+
+-- manipulation
+vim.g.mapleader = ' '
+vim.opt.clipboard:append{'unnamedplus'}
+vim.o.ttimeout = true
+vim.o.ttimeoutlen = 50
+
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require("nvim-tree").setup({
+        open_on_setup_file = true
+      })
+    end
+  }
+})
