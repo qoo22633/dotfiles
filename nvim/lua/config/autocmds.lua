e4d7327a0991b4233299ebcff7d6877c3d13787f
Yudai Yamashita <yama226.33@gmail.com>
2026-01-24 16:16:54 +0900

    RailsとReactが開発しやすくする

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,142 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Rails development autocmds
+local rails_group = vim.api.nvim_create_augroup("Rails", { clear = true })
+
+-- Set proper filetype for Rails files
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  group = rails_group,
+  pattern = {
+    "*.rb",
+    "*.rake",
+    "Gemfile",
+    "Rakefile",
+    "config.ru",
+    "*.gemspec"
+  },
+  command = "set filetype=ruby"
+})
+
+-- Set ERB filetype
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+  group = rails_group,
+  pattern = "*.erb",
+  command = "set filetype=eruby"
+})
+
+-- Auto-format Ruby files on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = rails_group,
+  pattern = "*.rb",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+-- Highlight Rails keywords
+vim.api.nvim_create_autocmd("FileType", {
+  group = rails_group,
+  pattern = "ruby",
+  callback = function()
+    vim.cmd([[
+      syntax keyword rubyRailsMethod
+        \ has_many belongs_to has_one has_and_belongs_to_many
+        \ validates validates_presence_of validates_uniqueness_of
+        \ before_save after_save before_create after_create
+        \ before_destroy after_destroy before_update after_update
+        \ scope default_scope where find_by find_by! create! update!
+        \ redirect_to render params session flash cookies request response
+        \ before_action after_action around_action skip_before_action
+        \ protect_from_forgery rescue_from respond_to format
+      highlight link rubyRailsMethod Function
+    ]])
+  end,
+})
+
+-- Set proper indentation for Ruby files
+vim.api.nvim_create_autocmd("FileType", {
+  group = rails_group,
+  pattern = {"ruby", "eruby"},
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- React/TypeScript development autocmds
+local react_group = vim.api.nvim_create_augroup("React", { clear = true })
+
+-- Auto-format TypeScript/JavaScript files on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = react_group,
+  pattern = {"*.ts", "*.tsx", "*.js", "*.jsx"},
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
+
+-- Set proper indentation for JS/TS files
+vim.api.nvim_create_autocmd("FileType", {
+  group = react_group,
+  pattern = {"javascript", "typescript", "javascriptreact", "typescriptreact"},
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- Auto-organize imports on save for TypeScript files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = react_group,
+  pattern = {"*.ts", "*.tsx"},
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+  end,
+})
+
+-- Highlight React/TypeScript keywords and hooks
+vim.api.nvim_create_autocmd("FileType", {
+  group = react_group,
+  pattern = {"typescript", "typescriptreact", "javascript", "javascriptreact"},
+  callback = function()
+    vim.cmd([[
+      syntax keyword reactKeywords useState useEffect useContext useReducer useCallback useMemo useRef
+      syntax keyword reactKeywords useImperativeHandle useLayoutEffect useDebugValue React ReactDOM
+      syntax keyword reactKeywords interface type extends implements export default import from as
+      highlight link reactKeywords Function
+    ]])
+  end,
+})
+
+-- Auto-create index.ts when creating new component directories
+vim.api.nvim_create_autocmd("BufNewFile", {
+  group = react_group,
+  pattern = "*/components/*/index.ts",
+  callback = function()
+    local dir = vim.fn.expand("%:h")
+    local component_name = vim.fn.fnamemodify(dir, ":t")
+    local content = string.format(
+      "export { default } from './%s';\nexport * from './%s';",
+      component_name,
+      component_name
+    )
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(content, "\n"))
+  end,
+})
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,68 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Rails development keymaps
+if vim.fn.isdirectory("app") == 1 then
+  -- Quick file navigation
+  map("n", "<leader>em", ":Emodel<space>", { desc = "Edit model" })
+  map("n", "<leader>ec", ":Econtroller<space>", { desc = "Edit controller" })
+  map("n", "<leader>ev", ":Eview<space>", { desc = "Edit view" })
+  map("n", "<leader>eh", ":Ehelper<space>", { desc = "Edit helper" })
+  map("n", "<leader>es", ":Espec<space>", { desc = "Edit spec" })
+  
+  -- Rails console
+  map("n", "<leader>rc", ":!rails console<cr>", { desc = "Rails console" })
+  map("n", "<leader>rs", ":!rails server<cr>", { desc = "Rails server" })
+  map("n", "<leader>rg", ":!rails generate<space>", { desc = "Rails generate" })
+  map("n", "<leader>rd", ":!rails destroy<space>", { desc = "Rails destroy" })
+  map("n", "<leader>rR", ":!bin/rails routes | grep<space>", { desc = "Search routes" })
+  
+  -- Database operations
+  map("n", "<leader>dm", ":!rails db:migrate<cr>", { desc = "Run migrations" })
+  map("n", "<leader>dr", ":!rails db:rollback<cr>", { desc = "Rollback migration" })
+  map("n", "<leader>ds", ":!rails db:seed<cr>", { desc = "Seed database" })
+  map("n", "<leader>dd", ":!rails db:drop<cr>", { desc = "Drop database" })
+  map("n", "<leader>dc", ":!rails db:create<cr>", { desc = "Create database" })
+end
+
+-- Ruby development keymaps
+map("n", "<leader>rb", ":!bundle install<cr>", { desc = "Bundle install" })
+map("n", "<leader>ru", ":!bundle update<cr>", { desc = "Bundle update" })
+
+-- React/TypeScript development keymaps
+if vim.fn.isdirectory("src") == 1 or vim.fn.filereadable("package.json") == 1 then
+  -- Component navigation
+  map("n", "<leader>cc", ":e src/components/", { desc = "Edit component" })
+  map("n", "<leader>ch", ":e src/hooks/", { desc = "Edit hook" })
+  map("n", "<leader>cp", ":e src/pages/", { desc = "Edit page" })
+  map("n", "<leader>cu", ":e src/utils/", { desc = "Edit utility" })
+  
+  -- TypeScript operations
+  map("n", "<leader>to", "<cmd>TypescriptOrganizeImports<cr>", { desc = "Organize imports" })
+  map("n", "<leader>tr", "<cmd>TypescriptRenameFile<cr>", { desc = "Rename file" })
+  map("n", "<leader>ta", "<cmd>TypescriptAddMissingImports<cr>", { desc = "Add missing imports" })
+  map("n", "<leader>tu", "<cmd>TypescriptRemoveUnused<cr>", { desc = "Remove unused imports" })
+  
+  -- React development shortcuts
+  map("n", "<leader>rfc", "i// React Functional Component<cr>import React from 'react';<cr><cr>interface Props {<cr>}<cr><cr>const ComponentName: React.FC<Props> = () => {<cr>return (<cr><div><cr></div><cr>);<cr>};<cr><cr>export default ComponentName;<esc>", { desc = "React FC snippet" })
+  
+  -- Jest/Testing shortcuts
+  map("n", "<leader>jt", "<cmd>!npm test -- --watchAll=false<cr>", { desc = "Run tests once" })
+  map("n", "<leader>jw", "<cmd>!npm test<cr>", { desc = "Run tests in watch mode" })
+  map("n", "<leader>jc", "<cmd>!npm test -- --coverage<cr>", { desc = "Run tests with coverage" })
+  
+  -- Development server
+  map("n", "<leader>ds", "<cmd>!npm run dev<cr>", { desc = "Start dev server" })
+  map("n", "<leader>db", "<cmd>!npm run build<cr>", { desc = "Build project" })
+  map("n", "<leader>dl", "<cmd>!npm run lint<cr>", { desc = "Run linter" })
+  map("n", "<leader>df", "<cmd>!npm run lint -- --fix<cr>", { desc = "Fix linting errors" })
+end
+
+-- Better terminal navigation for Rails
+map("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Terminal left" })
+map("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Terminal down" })
+map("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Terminal up" })
+map("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Terminal right" })
