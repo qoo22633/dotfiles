d1ca00a748bcbd3d1be716c0074cf537c694f3a9
Yudai Yamashita <yama226.33@gmail.com>
2026-02-03 23:24:19 +0900

    localleaderを設定することでocto.nvimを操作できるようにする

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- localleaderをカンマに設定
+vim.g.maplocalleader = ","
+
 -- 使用しないプロバイダーを無効化して警告を抑制
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
