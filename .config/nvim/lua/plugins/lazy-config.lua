fb376b062356c0f660e855de337237cbcfb54b27
Yudai Yamashita <yama226.33@gmail.com>
2026-04-13 10:25:42 +0900

    feat(nvim): lazy.nvimの設定をテーブルに統一し、conform.nvimを追加

--- .config/nvim/lua/plugins/lazy-config.lua
+++ .config/nvim/lua/plugins/lazy-config.lua
@@ -1,9 +1,19 @@
 return {
-  "folke/lazy.nvim",
-  opts = {
-    rocks = {
-      enabled = false,
-      hererocks = false,
+  {
+    "folke/lazy.nvim",
+    opts = {
+      rocks = {
+        enabled = false,
+        hererocks = false,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        yaml = {}, -- prettierによるシングルクォート変換を防ぐ
+      },
     },
   },
 }
